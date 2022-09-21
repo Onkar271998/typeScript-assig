959f010471ead152cb8dd71d87bdd05c3b32abbc
onkar <onkar271998@gmail.com>
2022-09-21 10:17:29 +0530

    typescript

---
+++ a.js
@@ -0,0 +1,40 @@
+// var a: number=100;
+// var b="abrakadabra";
+// let f1: number[]=[1,2,3,4]
+var Name = "Onkar";
+var age = 24;
+var isFetching = false;
+var array = [1, 2, 3, 4];
+var arrayofstrings = ["onkar", "deshmukh"];
+var Taple;
+Taple = [2, true, "ddcdvfbvv"];
+var data;
+(function (data) {
+    data["User"] = "onkar";
+    data["SuperUser"] = "onkarrr";
+    data["Admin"] = "onkarr";
+    data["SuperAdmin"] = "onkarrrr";
+})(data || (data = {}));
+function add(x, y) {
+    if (x === void 0) { x = 50; }
+    if (y === void 0) { y = 50; }
+    return console.log(x + y);
+}
+function divide(x, y) {
+    if (x === void 0) { x = 100; }
+    if (y === void 0) { y = 50; }
+    return console.log(x / y);
+}
+function names(x) {
+    if (x === void 0) { x = "onkar"; }
+    console.log(x);
+}
+function getName(_a) {
+    var fname = _a.fname, lname = _a.lname;
+    console.log(fname + " " + lname);
+}
+//  data=[
+//   const PersonDetails :{
+// }]
+// function Phonebook(PersonDetails){
+// }
---
+++ a.ts
@@ -0,0 +1,80 @@
+// var a: number=100;
+
+// var b="abrakadabra";
+
+// let f1: number[]=[1,2,3,4]
+
+let Name: string = "Onkar";
+
+let age: number = 24;
+
+let isFetching: boolean = false;
+
+let array: number[] = [1, 2, 3, 4];
+let arrayofstrings: string[] = ["onkar", "deshmukh"];
+
+let Taple: [number, boolean, string];
+Taple = [2, true, "ddcdvfbvv"];
+
+enum data {
+  User = "onkar",
+  SuperUser = "onkarrr",
+  Admin = "onkarr",
+  SuperAdmin = "onkarrrr",
+}
+
+function add(x: number = 50, y: number = 50) {
+  return console.log(x + y);
+}
+
+function divide(x: number = 100, y: number = 50) {
+  return console.log(x / y);
+}
+
+function names(x: string = "onkar") {
+  console.log(x);
+}
+
+
+interface User {
+  title: string;
+  status: boolean;
+  id: number;
+ 
+}    //not 
+
+
+function getName({fname,lname}){
+     console.log(fname+" "+lname)
+}
+
+
+interface Address {
+ 
+  houseNumber:number 
+  street:string
+  city:string
+  state:string
+  postalCode:number[] 
+  country:string
+ 
+}
+
+
+interface PersonDetails {
+  prefix:string;
+  phone:number[];
+  address:string[];
+  email:string;
+  firstname:string;
+  lastname:string;
+
+}
+
+//  data=[
+//   const PersonDetails :{
+
+// }]
+// function Phonebook(PersonDetails){
+
+// }
